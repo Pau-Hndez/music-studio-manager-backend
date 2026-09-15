5fb8142750e5b96a39a9726dc9ef927dd63cb5b3
Paula Hernandez <phernandez.musica@gmail.com>
2026-09-15 18:34:22 -0500

    Estructura inicial

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Music Studio Manager API");
+});
+
+const PORT = process.env.PORT || 3001;
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
